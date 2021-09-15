b48cbcc5357640844858ce95514c1ddfcf598a8d
Vee339 <81091817+Vee339@users.noreply.github.com>
2021-09-16 00:00:37 +0530

    The whole has been completed

---
+++ js/script.js
@@ -0,0 +1,3 @@
+document.querySelector("#home-page .header .hamburger").addEventListener("click",function(){
+      document.querySelector("#home-page .header .menu").classList.toggle("active");
+});
